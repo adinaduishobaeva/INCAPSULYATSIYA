57bb6ac45693490c8f78fbd6a2083b0af68c22cf
Адина <adina.duishobaeva.ch@gmail.com>
2024-01-22 23:03:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.name = "Marlen";
+        student.age = 26;
+        student.age= 344;
+        System.out.println(student.name);
+        System.out.println(student.age);
+
+        student.publicMethod();
+
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+        String name ;
+        int age ;
+
+        private void method(){ // PRIVATE бул класстын ичинде эле корунот
+                System.out.println("Hello world");
+        }
+        public void publicMethod(){ // PUBLIC бул публичный дегенди билдирет везде корунот 
+                System.out.println(" Салам ");
+        }
+}
